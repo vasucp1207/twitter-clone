7e25d506869f405e9ea6f594b876d360baeeeed2
Vasu <vasucp1207@gmail.com>
2022-07-14 20:59:24 +0530

    some small changes

---
+++ .history/pages/index_20220713184856.tsx
@@ -0,0 +1,32 @@
+import type { GetServerSideProps, NextPage } from 'next'
+import Head from 'next/head'
+import Feed from '../components/Feed'
+import Sidebar from '../components/Sidebar'
+import Widgets from '../components/Widgets'
+
+const Home: NextPage = () => {
+  return (
+    <div className='lg:max-w-6xl mx-auto max-h-screen overflow-hidden'>
+      <Head>
+        <title>Twitter App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className='grid grid-cols-9'>
+        <Sidebar />
+        <Feed />
+        <Widgets />
+      </main>
+    </div>
+  )
+}
+
+export default Home
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return (
+    props: {
+      
+    }
+  )
+}
